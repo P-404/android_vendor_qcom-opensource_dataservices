6079aff0af90715847f5e59adc32f76ceb8b681c
Jiju Kinattingal <quic_jijuk@quicinc.com>
2024-08-12 20:11:33 +0530

    automs: Fidl interface and auto generated files
    - This change adds the auto generated code from FIDLs
      and creates a lib out of it.
    
    Change-Id: I1177ff88689aa0051c094deaecf833b3482810a0

---
+++ automs/fidl/inc/ProxySvcBuilder.hpp
@@ -0,0 +1,22 @@
+/*
+* Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+* SPDX-License-Identifier: BSD-3-Clause-Clear
+*/
+
+#ifndef _PROXY_SVC_BUILDER_HPP_
+#define _PROXY_SVC_BUILDER_HPP_
+
+#include <CommonAPI/CommonAPI.hpp>
+#include <v0/com/qualcomm/qti/modem/InfoSvcProxy.hpp>
+#include <v0/com/qualcomm/qti/modem/RadioSvcProxy.hpp>
+#include <v0/com/qualcomm/qti/modem/SimSvcProxy.hpp>
+
+class ProxySvcBuilder {
+   public:
+    static void init();
+    static std::shared_ptr<v0::com::qualcomm::qti::modem::SimSvcProxy<>> getSimSvcProxy();
+    static std::shared_ptr<v0::com::qualcomm::qti::modem::RadioSvcProxy<>> getRadioSvcProxy();
+    static std::shared_ptr<v0::com::qualcomm::qti::modem::InfoSvcProxy<>> getInfoSvcProxy();
+};
+
+#endif  // _PROXY_SVC_BUILDER_HPP_
---
+++ automs/fidl/src/ProxySvcBuilder.cpp
@@ -0,0 +1,44 @@
+/*
+* Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+* SPDX-License-Identifier: BSD-3-Clause-Clear
+*/
+
+#include <ProxySvcBuilder.hpp>
+#include <chrono>
+#include <thread>
+
+using namespace v0::com::qualcomm::qti::modem;
+
+void ProxySvcBuilder::init() {
+    // commonapi proxy init
+    CommonAPI::Runtime::setProperty("LogContext", "E01C");
+    CommonAPI::Runtime::setProperty("LogApplication", "E01C");
+    CommonAPI::Runtime::setProperty("LibraryBase", "TelephonyInterface");
+}
+
+std::shared_ptr<SimSvcProxy<>> ProxySvcBuilder::getSimSvcProxy() {
+    auto simProxy = CommonAPI::Runtime::get()->buildProxy<SimSvcProxy>("local", "modem.SimSvc",
+                                                                       "SimSvcTest");
+    while (!simProxy->isAvailable()) {
+        std::this_thread::sleep_for(std::chrono::microseconds(10));
+    }
+    return simProxy;
+}
+
+std::shared_ptr<RadioSvcProxy<>> ProxySvcBuilder::getRadioSvcProxy() {
+    auto radioProxy = CommonAPI::Runtime::get()->buildProxy<RadioSvcProxy>(
+            "local", "modem.RadioSvc", "radioSvcTest");
+    while (!radioProxy->isAvailable()) {
+        std::this_thread::sleep_for(std::chrono::microseconds(10));
+    }
+    return radioProxy;
+}
+
+std::shared_ptr<InfoSvcProxy<>> ProxySvcBuilder::getInfoSvcProxy() {
+    auto infoProxy = CommonAPI::Runtime::get()->buildProxy<InfoSvcProxy>("local", "modem.InfoSvc",
+                                                                         "InfoSvcTest");
+    while (!infoProxy->isAvailable()) {
+        std::this_thread::sleep_for(std::chrono::microseconds(10));
+    }
+    return infoProxy;
+}
